f15455e7f7787bfa5d229586c78f01154cd102e0
tungdtran <twotimestung@gmail.com>
2022-07-10 16:38:07 -0700

    Fixed KC_INS typo

--- keyboards/peej/lumberjack/keymaps/tungdtran/keymap.c
+++ keyboards/peej/lumberjack/keymaps/tungdtran/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, KC_LCBR, KC_RCBR, _______,
     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_UNDS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_IN,   KC_HOME, KC_PGUP, KC_PSCR, KC_SCRL, KC_PAUS, _______, KC_NUHS, KC_NUBS, _______, _______, _______,
+    KC_INS,  KC_HOME, KC_PGUP, KC_PSCR, KC_SCRL, KC_PAUS, _______, KC_NUHS, KC_NUBS, _______, _______, _______,
     KC_DEL,  KC_END,  KC_PGDN, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
